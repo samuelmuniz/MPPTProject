cf63909a8d8dcd0cdeae6fd0845aaa9e3c5efc11
Samuel Muniz Silva <67869454+samuelmuniz@users.noreply.github.com>
2023-06-20 20:39:38 +0200

    Update MPPTProject.ino
    Change of method from the "touch and observe" to the "dpower/dv"

--- MPPTProject.ino
+++ MPPTProject.ino
@@ -1,69 +1,90 @@
+
+#include <LiquidCrystal_I2C.h>
+
+
+
 #define currentSensor 25
-#define dutyCyclePWM 26
-#define inputVoltageSensor 27
+#define inputVoltageSensor 26
+#define dutyCyclePWM 27
 
 /*
 current sensor model  = ACS712x20A
  - current range = +- 20 A
  - 
-
-Boost DC-DC Converter
+Buck-Boost DC-DC Converter
 - 380 V output
-
 */
 
-const int sensitivity = 10; //10 A for 1 V
-const int freq = 5000; // PWM frequency
+// PWM frequency
+const int freq = 5000; 
 const int ledChannel = 0;
-const int resolution = 13; // resolution used for the PWM sugnal
-int dutyCycleMain = 2048;
-int dutyCycleTest = 2048;
-float outputVolt = 0;
+const int timeDelay = 500;
+// resolution used for the PWM signal
+//PWM max value = 4096 with resolution = 12
+const int resolution = 12; 
+int dutyCycle = 2048;
 float inputVolt = 0;
-float current;
-float power[3]={0};
+float current = 0;
+float power[2]={0};
+float dpower = 0;
 
 unsigned long t = 0;
 
+
+
+void delay(){
+  t = millis();
+  while(millis() - t <= timeDelay){ 
+  }
+}
+
+void measurement(){
+  // Ip = [-20 A,20 A]
+  current = map(analogRead(currentSensor),0,4095,-2000,2000);
+  current = current/100.00; 
+  // resolution ADC ESP32 = 12 bits
+  inputVolt = map(analogRead(inputVoltageSensor),0,4095,0,1000); 
+  // V = [0 V,10 V]
+  inputVolt = inputVolt/100.00; 
+}
 
 
 
 void setup() {
   // put your setup code here, to rfun once:
   Serial.begin(115200);
-  pinMode(currentSensor,OUTPUT);
-  pinMode(dutyCyclePWM, INPUT_PULLUP);
-    // configure LED PWM functionalitites
+  pinMode(dutyCyclePWM,OUTPUT);
+  pinMode(currentSensor, INPUT_PULLUP);
+  pinMode(inputVoltageSensor, INPUT_PULLUP);
+  // configure LED PWM functionalitites
   ledcSetup(ledChannel, freq, resolution);
-  
   // attach the channel to the GPIO to be controlled
-  ledcAttachPin(currentSensor, ledChannel);
+  ledcAttachPin(dutyCyclePWM, ledChannel);
 }
 
 void loop() {
 
-  for(int i=0;i<=2;i++){
-    dutyCycleTest = (dutyCycleMain - 10) + i*10;
-    ledcWrite(ledChannel, dutyCycleTest);
-    while(millis()-t <= 10){ // rotine used for waiting the stationary state
-    }
-    t = millis();
-    current = map(analogRead(currentSensor),0,4095,-250,250)/100.0; // resolution ADC ESP32 = 12 bits
-    inputVolt = map(analogRead(inputVoltageSensor),0,4095,0,1000); // resolution ADC ESP32 = 12 bits
-    power[i] = current*inputVolt;
-  }
-
-  if(power[1] <= power[0] && power[1] >= power[2]){
-    dutyCycleMain -= 10;  
-  }
-  else if(power[1] >= power[0] && power[1] <= power[2]){
-    dutyCycleMain += 10;  
-  }
-
-
-
-
+  measurement();
+  power[0] = current*inputVolt;
+  ledcWrite(ledChannel, dutyCycle + 10);
   
+  delay();
+
+  measurement();
+  power[1] = current*inputVolt;
+  ledcWrite(ledChannel, dutyCycle);
+
+  dpower = power[1] - power[0];
+
+  if(dpower > 0 && dutyCycle < 3000){
+    dutyCycle += 10;  
+  }
+  else if(dpower < 0 && dutyCycle > 500){
+    dutyCycle -= 10;  
+  }
+
+  ledcWrite(ledChannel, dutyCycle);
+  delay();
 
 
 }
